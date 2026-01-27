4c2dfda11622b786b8a7f5f65beee9d010f8a179
albus.kim <albus.kim@albuskimui-MacBookPro.local>
2026-01-27 17:51:33 +0900

    feat: AnswerNumberModel

---
+++ src/main/java/model/AnswerNumberModel.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.Arrays;
+
+public class AnswerNumberModel {
+    private static final int ANSWER_NUMBER_LENGTH = 3;
+    private int[] answerNumber;
+
+    public AnswerNumberModel(int[] answerNumber) {
+        if (!validate(answerNumber)) {
+            throw new IllegalArgumentException("Invalid answer number");
+        }
+        this.answerNumber = answerNumber;
+    }
+
+    public Boolean validate(int[] answerNumber) {
+        return isLengthValid(answerNumber) && isAllDistinct(answerNumber);
+    }
+
+    private Boolean isLengthValid(int[] answerNumber) {
+        return answerNumber.length == ANSWER_NUMBER_LENGTH;
+    }
+
+    private Boolean isAllDistinct(int[] answerNumber) {
+        return Arrays.stream(answerNumber).distinct().count() == ANSWER_NUMBER_LENGTH;
+    }
+
+    public int[] getAnswerNumber() {
+        return answerNumber;
+    }
+}
