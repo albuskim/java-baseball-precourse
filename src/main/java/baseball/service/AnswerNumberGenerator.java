f4af9c4b597a870b90574bc821ab62f9466ce161
albus.kim <albus.kim@albuskimui-MacBookPro.local>
2026-01-28 18:28:33 +0900

    fix: correct random answer generation logic

--- src/main/java/baseball/service/AnswerNumberGenerator.java
+++ src/main/java/baseball/service/AnswerNumberGenerator.java
@@ -2,15 +2,18 @@ package baseball.service;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 public class AnswerNumberGenerator {
     public static final int ANSWER_NUMBER_LENGTH = 3;
     public int[] generateAnswerNumber() {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        Collections.shuffle(Arrays.asList(numbers));
-        int[] answerNumber = Arrays.copyOfRange(
-            numbers, 0, ANSWER_NUMBER_LENGTH
-        );
+        Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        List<Integer> numberList = Arrays.asList(numbers);
+        Collections.shuffle(numberList);
+        int[] answerNumber = new int[ANSWER_NUMBER_LENGTH];
+        for (int i = 0; i < ANSWER_NUMBER_LENGTH; i++) {
+            answerNumber[i] = numberList.get(i);
+        }
         return answerNumber;
     }
 }
