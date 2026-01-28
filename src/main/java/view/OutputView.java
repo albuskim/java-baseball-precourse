9f69a1b0202fdd4b1ac45069c9034c6069fa4f0e
albus.kim <albus.kim@albuskimui-MacBookPro.local>
2026-01-28 15:50:22 +0900

    feat: OutputView

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,13 @@
+package view;
+
+import model.GameResultModel;
+
+public class OutputView {
+    public void printResult(GameResultModel gameResult) {
+        System.out.println(gameResult.toString());
+    }
+
+    public void printContinueQuestion() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
