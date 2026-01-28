3fc53d1d201ad263b7420df1c706ed33edb7aab1
albus.kim <albus.kim@albuskimui-MacBookPro.local>
2026-01-28 17:27:02 +0900

    fix: correct answer number range

--- src/main/java/service/AnswerNumberGenerator.java
+++ src/main/java/service/AnswerNumberGenerator.java
@@ -7,7 +7,7 @@ import model.AnswerNumberModel;
 
 public class AnswerNumberGenerator {
     public int[] generateAnswerNumber() {
-        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         Collections.shuffle(Arrays.asList(numbers));
         int[] answerNumber = Arrays.copyOfRange(
             numbers, 0, AnswerNumberModel.ANSWER_NUMBER_LENGTH
